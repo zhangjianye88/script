c12ccc98c6c4e2f9be6b1c596814cdd87d47ca45
Jacky <zhangjianye@hotmail.com>
2018-07-03 18:29:39 +0800

    update website check file

--- check_csdn_working.py
+++ check_csdn_working.py
@@ -1,18 +1,14 @@
-'''
-Created on Sep 6, 2015
 
-@author: jie
-'''
 import requests
 import sys
 
-WEB_URL = 'http://www.csdn.net/'
+WEB_URL = 'http://www.csdn.com/'
 
 
 def run():
     try:
         page = requests.get(WEB_URL)
-        if page.status_code == 200 and page.content.decode("utf-8").find('<title>CSDN.NET - 全球最大中文IT社区，为IT专业技术人员提供最全面的信息传播和服务平台</title>') >= 0:
+        if page.status_code == 200 and page.content.decode("utf-8").find('<title>CSDN-专业IT技术社区</title>') >= 0:
             print("CSDN is working great :)")
         else:
             print("It looks like CSDN is having trouble, some one please take a look at it")
